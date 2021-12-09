36a15e6004eb8ecbd03adf590de9bd8a13644eed
Thuan Pham <phamtanthuan2311@gmail.com>
2021-12-09 09:00:39 +0700

    [👌Auto commit] Last time I said it works? I was kidding.  Try this.

--- lab_7_javascript/script.js
+++ lab_7_javascript/script.js
@@ -3,17 +3,31 @@ document.getElementById("buttonCong").onclick = function cong() {
   const so2 = document.getElementById("so2").value;
   let flag = true;
 
-  if (so1.isNaN === false || so1 === "") {
+  if (isNaN(so1) === true || so1 === "") {
     alert("Nhập số ở ô số 1 đi nha: ");
-    so1.focus();
+    document.getElementById("so1").style.color = "red";
+    document.getElementById("so1").focus();
     flag = false;
-  } else if (so2.isNaN === false || so2 === "") {
+  } else if (isNaN(so2) === true || so2 === "") {
     alert("Nhập số ở ô số 2 đi nha: ");
-    so2.focus();
+    document.getElementById("so1").style.color = "red";
+    document.getElementById("so2").focus();
     flag = false;
   }
 
   if (flag === true) {
     document.getElementById("ketQua").value = Number(so1) + Number(so2);
+    document.getElementById("so1").style.color = "black";
+    document.getElementById("so2").style.color = "black";
   }
 };
+
+function randomBackgroundColor() {
+  let x = Math.floor(Math.random() * 256);
+  let y = Math.floor(Math.random() * 256);
+  let z = Math.floor(Math.random() * 256);
+  console.log(x, y, z);
+  document.body.style.background = "rgb(" + x + "," + y + "," + z + ")";
+}
+
+setTimeout(randomBackgroundColor(), 1000);
