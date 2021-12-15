0c7d0e2c1d5223aebf8e8e2f549f0cba93d1a2d3
Thuan Pham <phamtanthuan2311@gmail.com>
2021-12-15 10:07:35 +0700

    [👌Auto commit] I just wanna tell you how I'm feeling

--- lab_7_javascript/lab7_bai4.js
+++ lab_7_javascript/lab7_bai4.js
@@ -2,21 +2,36 @@ let index = 1;
 
 function hinh(i) {
   index = i;
-  document.getElementById("showImg").src = "./img/" + i + ".png";
+  document.getElementById("showImg").src = "img/" + i + ".png";
 }
 
-function hinhtruoc() {
+document.getElementById("buttonImg1").onclick = function () {
+  index = 1;
+  hinh(index);
+};
+
+document.getElementById("buttonImg2").onclick = function () {
+  index = 2;
+  hinh(index);
+};
+
+document.getElementById("buttonImg3").onclick = function () {
+  index = 3;
+  hinh(index);
+};
+
+document.getElementById("buttonNextImg").onclick = function hinhtruoc() {
   index--;
   if (index < 1) {
     index = 3;
   }
-  document.getElementById("showImg").src = "./img/" + index + ".png";
-}
+  document.getElementById("showImg").src = "img/" + index + ".png";
+};
 
-function hinhsau() {
+document.getElementById("buttonPrevImg").onclick = function hinhsau() {
   index++;
   if (index > 3) {
     index = 1;
   }
-  document.getElementById("showImg").src = "./img/" + index + ".png";
-}
+  document.getElementById("showImg").src = "img/" + index + ".png";
+};
