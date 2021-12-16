5e6d8ba725c186ca94a39b5ab2f9122921193150
Thuan Pham <phamtanthuan2311@gmail.com>
2021-12-16 08:00:38 +0700

    [👌Auto commit] Well, it's doing something.

---
+++ lab8/script.js
@@ -0,0 +1,26 @@
+function randomNumber() {
+  return Math.floor(Math.random() * 100) + 1;
+}
+
+let randomNumber1Value = document.getElementById("randomNumber1").value;
+let randomNumber2Value = document.getElementById("randomNumber1").value;
+let sumRandomNumber = 0;
+let inputResultValue = document.getElementById("inputResult").value;
+
+document.getElementById("buttonNew").onclick = function () {
+  document.getElementById("randomNumber1").value = randomNumber();
+  document.getElementById("randomNumber2").value = randomNumber();
+  sumRandomNumber = randomNumber1Value + randomNumber2Value;
+};
+
+function checkResult() {
+  if (inputResultValue === sumRandomNumber) {
+    document.getElementById("result").valuestyle.color = green;
+    document.getElementById("result").innerHTML = "Đúng";
+  } else {
+    document.getElementById("result").value.style.color = red;
+    document.getElementById("result").innerHTML = "Sai";
+  }
+}
+
+checkResult();
