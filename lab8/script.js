dc7f834d086174f55dafbf65156f44064876a9b6
Thuan Pham <phamtanthuan2311@gmail.com>
2021-12-16 08:47:58 +0700

    [👌Auto commit] oops, forgot to add the file

--- lab8/script.js
+++ lab8/script.js
@@ -1,26 +1,39 @@
-function randomNumber() {
-  return Math.floor(Math.random() * 100) + 1;
-}
-
-let randomNumber1Value = document.getElementById("randomNumber1").value;
-let randomNumber2Value = document.getElementById("randomNumber1").value;
+let randomNumber1Value = 0;
+let randomNumber2Value = 0;
 let sumRandomNumber = 0;
-let inputResultValue = document.getElementById("inputResult").value;
 
 document.getElementById("buttonNew").onclick = function () {
-  document.getElementById("randomNumber1").value = randomNumber();
-  document.getElementById("randomNumber2").value = randomNumber();
+  randomNumber1Value = Math.floor(Math.random() * 100) + 1;
+  document.getElementById("randomNumber1").value = randomNumber1Value;
+
+  randomNumber2Value = Math.floor(Math.random() * 100) + 1;
+  document.getElementById("randomNumber2").value = randomNumber2Value;
+
   sumRandomNumber = randomNumber1Value + randomNumber2Value;
 };
 
-function checkResult() {
-  if (inputResultValue === sumRandomNumber) {
-    document.getElementById("result").innerHTML = "Đúng";
-    document.getElementById("result").valuestyle.color = green;
+document.getElementById("buttonCheckResult").onclick = function () {
+  let inputResultValue = document.getElementById("inputResult").value;
+
+  if (inputResultValue == sumRandomNumber) {
+    document.getElementById("result").innerHTML =
+      "<span style='color: green'>Đúng</span>";
   } else {
-    document.getElementById("result").innerHTML = "Sai";
-    document.getElementById("result").value.style.color = red;
+    document.getElementById("result").innerHTML =
+      "<span style='color: red'>Sai</span>";
   }
+};
+
+let a = new Array();
+for (let i = 0; i < 10; i++) {
+  a[i] = Math.floor(Math.random() * 10) + 1;
+  document.write(a[i] + " ");
 }
 
-checkResult();
+document.write("<br/>");
+
+document.write("<p>mảng đảo ngược là:</p>");
+
+for (i = 9; i >= 0; i--) {
+  document.write(a[i] + " ");
+}
