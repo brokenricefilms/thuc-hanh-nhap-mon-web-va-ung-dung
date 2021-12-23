1904cd0bf2050e4c683f2393b13093f6ebe7cfb3
Thuan Pham <phamtanthuan2311@gmail.com>
2021-12-23 08:23:37 +0700

    [👌Auto commit] The same thing we do every night, Pinky - try to take over the world!

--- De02_PhamTanThuan/js/script.js
+++ De02_PhamTanThuan/js/script.js
@@ -1,26 +1,42 @@
 function summit() {
-  let flag = true;
+  let flag = 0;
+  let flag2 = 0;
+  let flag3 = 0;
+  let flag4 = 0;
   let password = document.getElementById("password").value;
   let confirm = document.getElementById("confirm_password").value;
   let name = document.getElementById("name").value;
   let email = document.getElementById("email").value;
 
+  if (password == "" && confirm_password == "") {
+    flag4 = 0;
+    alert("không được để trống password");
+  } else {
+    flag4 = 1;
+  }
+
   if (confirm != password) {
+    flag = 0;
     alert("password không trùng nhau nhập lại");
-    flag == false;
+  } else {
+    flag = 1;
   }
 
   if (name == "") {
+    flag2 = 0;
     alert("không được để trống họ tên");
-    flag == false;
+  } else {
+    flag2 = 1;
   }
 
   if (email == "") {
+    flag3 = 0;
     alert("không được để trống email");
-    flag == false;
+  } else {
+    flag3 = 1;
   }
 
-  if (flag == true) {
+  if (flag + flag2 + flag3 + flag4 == 4) {
     window.location.href = "../index.html";
   }
 }
